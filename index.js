754a562ece1d6b2715bcaf2e941ca63863f02db0
MANIVENKATRAJ M <127532509+mp143mani@users.noreply.github.com>
2023-08-27 08:18:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
 
 
-const http = require('http');
-const fs = require('fs');
+// const http = require('http');
+// const fs = require('fs');
 
-const server = http.createServer((req, res) => {
+// const server = http.createServer((req, res) => {
+//     const fileName = `files/${new Date().toLocaleString().replace(/[/: ,]/g, "_")}.txt`;
+//     const fileContent = `Now This Time is - ${new Date().toLocaleString()}`;
+
+//     fs.writeFile(fileName, fileContent, 'utf-8', (error) => {
+//         if (error) {
+//             console.log(error);
+//             res.writeHead(500, { 'Content-Type': 'text/html' });
+//             res.end('An error occurred');
+//         } else {
+//             fs.readFile(fileName, 'utf-8', (error, data) => {
+//                 if (error) {
+//                     console.log(error);
+//                     res.writeHead(500, { 'Content-Type': 'text/html' });
+//                     res.end('An error occurred');
+//                 } else {
+//                     res.writeHead(200, { 'Content-Type': 'text/html' });
+//                     res.end(data);
+//                 }
+//             });
+//         }
+//     });
+// });
+
+// server.listen(21000, () => console.log("Server listening on port 21000"));
+
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 21000;
+
+app.get('/', (req, res) => {
     const fileName = `files/${new Date().toLocaleString().replace(/[/: ,]/g, "_")}.txt`;
     const fileContent = `Now This Time is - ${new Date().toLocaleString()}`;
 
     fs.writeFile(fileName, fileContent, 'utf-8', (error) => {
         if (error) {
             console.log(error);
-            res.writeHead(500, { 'Content-Type': 'text/html' });
-            res.end('An error occurred');
+            res.status(500).send('An error occurred');
         } else {
             fs.readFile(fileName, 'utf-8', (error, data) => {
                 if (error) {
                     console.log(error);
-                    res.writeHead(500, { 'Content-Type': 'text/html' });
-                    res.end('An error occurred');
+                    res.status(500).send('An error occurred');
                 } else {
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.end(data);
+                    res.status(200).send(data);
                 }
             });
         }
     });
 });
 
-server.listen(21000, () => console.log("Server listening on port 21000"));
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
